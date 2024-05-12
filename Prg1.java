4209f27657b9b2f50d07258603dd7100a8f6bc92
Batool Amina <85053802+BatoolAmina@users.noreply.github.com>
2024-05-12 13:33:08 +0530

    Create Prg1.java

---
+++ Prg1.java
@@ -0,0 +1,20 @@
+//2 numbers to add in applet using Event Handling
+import java.applet.*;  
+import java.awt.*;  
+import java.awt.event.*;  
+public class EventApplet extends Applet implements ActionListener{  
+Button b;  
+TextField tf;  
+public void init(){  
+tf=new TextField();  
+tf.setBounds(30,40,150,20);  
+b=new Button("Click");  
+b.setBounds(80,150,60,50);  
+add(b);add(tf);  
+b.addActionListener(this);  
+setLayout(null);  
+}  
+public void actionPerformed(ActionEvent e){  
+ tf.setText("Welcome");  
+}   
+}
